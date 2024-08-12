ce5f251490367001949e01f2b755ee5e310601be
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2024-08-12 10:31:39 +0200

    sout

--- src/main/java/ecostruxure/rate/calculator/gui/component/modals/addteam/AddTeamController.java
+++ src/main/java/ecostruxure/rate/calculator/gui/component/modals/addteam/AddTeamController.java
@@ -54,6 +54,7 @@ public class AddTeamController implements ModalController {
         Task<Boolean> saveTask = new Task<>() {
             @Override
             protected Boolean call() {
+                System.out.println(interactor.addTeam());
                 return interactor.addTeam();
             }
         };
