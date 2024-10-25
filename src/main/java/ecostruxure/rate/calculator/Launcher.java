1568f86d70a214694dd9f5996f274b7d8d463331
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2024-10-25 10:05:34 +0200

    bug:fixdet

--- src/main/java/ecostruxure/rate/calculator/Launcher.java
+++ src/main/java/ecostruxure/rate/calculator/Launcher.java
@@ -1,6 +1,18 @@
 package ecostruxure.rate.calculator;
 
-import atlantafx.base.theme.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "ecostruxure.rate.calculator.controllers")
+public class Launcher {
+    public static void main(String[] args) {
+        SpringApplication.run(Launcher.class, args);
+    }
+}
+
+/*import atlantafx.base.theme.*;
 import ecostruxure.rate.calculator.gui.component.main.MainController;
 import ecostruxure.rate.calculator.gui.util.constants.LocalizedText;
 import ecostruxure.rate.calculator.gui.widget.world.WorldWidget;
@@ -17,11 +29,14 @@ import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
 import java.util.Locale;
 import java.util.Objects;
-import java.util.Properties;
+import java.util.Properties;*/
 
+
+/*
 public class Launcher extends Application {
     public static void main(String[] args) {
         Application.launch();
@@ -54,81 +69,4 @@ public class Launcher extends Application {
         scene.setRoot(mainController.view());
         stage.show();
     }
-}
-
-//public class Launcher extends Application {
-//    private Stage stage;
-//
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
-//
-//    @Override
-//    public void start(Stage stage) {
-//        Application.setUserAgentStylesheet(new PrimerLight().getUserAgentStylesheet());
-//        this.stage = stage;
-//        initUI();
-//    }
-//
-//    private void initUI() {
-//        try {
-//            var settingsFile = AppConfig.load(AppConfig.SETTINGS_FILE);
-//            var userLanguage = settingsFile.getProperty(AppConfig.USER_LANGUAGE);
-//            if (userLanguage.contains(AppConfig.USER_LANGUAGE_DA)) {
-//                LocalizedText.CURRENT_LOCALE.set(Locale.of(userLanguage));
-//            } else {
-//                LocalizedText.CURRENT_LOCALE.set(Locale.ENGLISH);
-//            }
-//
-////            var userTheme = settingsFile.getProperty(AppConfig.USER_THEME);
-////            switch (userTheme) {
-////                case AppConfig.USER_THEME_PRIMER -> Application.setUserAgentStylesheet(new PrimerLight().getUserAgentStylesheet());
-////                case AppConfig.USER_THEME_NORD -> Application.setUserAgentStylesheet(new NordLight().getUserAgentStylesheet());
-////                case AppConfig.USER_THEME_CUPERTINO -> Application.setUserAgentStylesheet(new CupertinoDark().getUserAgentStylesheet());
-////                default -> Application.setUserAgentStylesheet(new PrimerLight().getUserAgentStylesheet());
-////            }
-//        } catch (IOException e) {
-//            LocalizedText.CURRENT_LOCALE.set(Locale.ENGLISH);
-//        }
-//
-//        Scene splashScene = new Scene(createSplashScreen(), 1340, 940);
-//
-//        stage.setTitle("Loading...");
-//        var icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/app_icon.png")));
-//        stage.getIcons().add(icon);
-//        stage.setScene(splashScene);
-//        stage.show();
-//
-//        new Thread(() -> {
-//            MainController mainController = setupMainController();
-//            Platform.runLater(() -> {
-//                Scene mainScene = new Scene(mainController.view(), 1340, 940);
-//                stage.setScene(mainScene);
-//                stage.setTitle("EcoStruxure - Rate Calculator");
-//
-//                stage.show();
-//            });
-//        }).start();
-//    }
-//
-//    private Region createSplashScreen() {
-//        Image logo = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/sidebar_logo.png")));
-//        ImageView logoView = new ImageView(logo);
-//
-//        Label loadingLabel = new Label("Loading... Please wait.");
-//        loadingLabel.setStyle("-fx-font-size: 20px; -fx-text-fill: white;");
-//
-//        ProgressIndicator progressIndicator = new ProgressIndicator();
-//        progressIndicator.setStyle("-fx-pref-width: 50px; -fx-pref-height: 50px;");
-//
-//        VBox splashLayout = new VBox(20);
-//        splashLayout.setAlignment(Pos.CENTER);
-//        splashLayout.getChildren().addAll(logoView, loadingLabel, progressIndicator);
-//        splashLayout.setStyle("-fx-padding: 30; -fx-background-color: #6BAE61; -fx-alignment: center;");
-//        return splashLayout;
-//    }
-//
-//    private MainController setupMainController() {
-//        return new MainController(stage);
-//    }
-//}
+}*/
---
+++ src/main/java/ecostruxure/rate/calculator/controllers/ProfileController.java
@@ -0,0 +1,22 @@
+package ecostruxure.rate.calculator.controllers;
+
+import ecostruxure.rate.calculator.be.Profile;
+import ecostruxure.rate.calculator.bll.service.ProfileService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/profile")
+public class ProfileController {
+    private final ProfileService profileService;
+
+    public ProfileController() throws Exception {
+        this.profileService = new ProfileService();
+    }
+
+    @GetMapping("/{id}")
+    public Profile getProfile(@PathVariable UUID profileId) throws Exception {
+        return profileService.get(profileId);
+    }
+}
