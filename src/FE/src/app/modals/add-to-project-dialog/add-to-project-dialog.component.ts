249f36d33e185c9a1306d8678a2d5dc87186b17b
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2024-11-19 09:47:28 +0100

    feat(): Sender den videre til en kyndig

--- src/FE/src/app/modals/add-to-project-dialog/add-to-project-dialog.component.ts
+++ src/FE/src/app/modals/add-to-project-dialog/add-to-project-dialog.component.ts
@@ -2,7 +2,7 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Project, ProjectMembers, Team} from "../../models";
 import {TranslateModule, TranslateService} from "@ngx-translate/core";
 import {MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle} from "@angular/material/dialog";
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatIcon} from "@angular/material/icon";
 import {MatListOption, MatSelectionList, MatSelectionListChange} from "@angular/material/list";
 import {MatFormField, MatInputModule} from "@angular/material/input";
@@ -24,7 +24,8 @@ import {SnackbarService} from '../../services/snackbar.service';
     MatDialogActions,
     MatDialogClose,
     MatInputModule,
-    MatDialogTitle
+    MatDialogTitle,
+    FormsModule
   ],
   templateUrl: './add-to-project-dialog.component.html',
   styleUrl: './add-to-project-dialog.component.css'
@@ -47,23 +48,27 @@ export class AddToProjectDialogComponent implements OnInit {
   async ngOnInit() {
     this.teamForm = this.formBuilder.group({
       teamName: ['', Validators.required],
-      teams: [[], Validators.required]
+      teams: [[], Validators.required],
+      projectAllocations: this.formBuilder.array([])
     })
 
     let teams = await this.teamService.getTeams();
-    this.teamList = teams.filter(team => !this.project.projectMembers.some(member => member.teamId === team.teamId));
+    this.teamList = teams.filter(team =>
+      !this.project.projectMembers.some(member =>
+        member.teamId === team.teamId));
   }
 
   async onSave() {
+    const projectAllocations = this.teamForm.get('projectAllocations')?.value;
     let teams = this.selectedTeams;
-    teams.forEach( team => {
+    teams.forEach( (team, projectAllocationsIndex) => {
       let projectMember: ProjectMembers = {
         projectId: '',
         teamId: team.teamId!,
         name: team.name,
         dayRate: team.dayRate,
         markup: team.markup,
-        projectAllocation: 0
+        projectAllocation: projectAllocations[projectAllocationsIndex]  || 0
       }
       this.project.projectMembers.push(projectMember);
     });
@@ -78,5 +83,10 @@ export class AddToProjectDialogComponent implements OnInit {
 
   onSelectionChange($event: MatSelectionListChange) {
     this.selectedTeams = $event.source.selectedOptions.selected.map(team => team.value);
+    const projectAllocations = this.teamForm.get('projectAllocations') as FormArray;
+    projectAllocations.clear();
+    this.selectedTeams.forEach(() => {
+      projectAllocations.push(this.formBuilder.control(0, [Validators.required, Validators.min(0), Validators.min(100)]));
+    });
   }
 }
--- src/FE/src/app/pages/project-page/project-page.component.ts
+++ src/FE/src/app/pages/project-page/project-page.component.ts
@@ -184,6 +184,6 @@ export class ProjectPageComponent implements OnInit{
   }
 
   onRemove() {
-    
+
   }
 }
