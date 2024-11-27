8ca7923dbab158ddc90c7ac7fc1e80e864f9eea1
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2024-11-27 09:32:44 +0100

    fix(backend): last update in project_team_data

--- db-init/2_insert_data/07_project_team_data.sql
+++ db-init/2_insert_data/07_project_team_data.sql
@@ -3,7 +3,7 @@ INSERT INTO dbo.project_members (project_team_id,
                                  teams_id,
                                  allocation_on_project,
                                  day_rate_on_project)
-VALUES  ('',
+VALUES  ('17b1adfd-778e-4d2f-a2f7-903ae2e36f48',
          '769f922a-6e19-40d5-b46d-8ebd43960736',
          '937b379c-c326-4db2-9575-8313f59ddf2c',
          100);
