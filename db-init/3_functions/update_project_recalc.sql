b801cf62a8597662de1d455817b91b6a4daf7c82
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2024-11-28 15:59:28 +0100

    feat(sql): update_project_recalc working 90%.
    Missing how to calculate week days, else working properly

---
+++ db-init/3_functions/update_project_recalc.sql
@@ -0,0 +1,35 @@
+CREATE OR REPLACE FUNCTION update_project_recalc()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    IF NEW.project_end_date IS DISTINCT FROM OLD.project_end_date OR
+       NEW.project_start_date IS DISTINCT FROM OLD.project_start_date THEN
+        NEW.project_total_days := NEW.project_end_date - NEW.project_start_date;
+    END IF;
+    IF NEW.project_price IS DISTINCT FROM OLD.project_price THEN
+        IF NEW.project_price IS NOT NULL THEN
+
+            IF CURRENT_DATE < NEW.project_start_date THEN
+                NEW.project_gross_margin :=
+                        ((NEW.project_price - (NEW.project_day_rate * NEW.project_total_days)) / NEW.project_price) *
+                        100;
+            ELSE
+                NEW.project_gross_margin := ((NEW.project_price - COALESCE(NEW.project_total_cost_at_change, 0) -
+                                              (NEW.project_day_rate * (NEW.project_end_date -
+                                                                       COALESCE(NEW.project_rest_cost_date,
+                                                                                NEW.project_start_date)))) /
+                                             NEW.project_price) * 100;
+            END IF;
+        ELSE
+            NEW.project_gross_margin := 0;
+        END IF;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER project_update_trigger_recalc
+    BEFORE UPDATE
+    ON project
+    FOR EACH ROW
+EXECUTE FUNCTION update_project_recalc();
