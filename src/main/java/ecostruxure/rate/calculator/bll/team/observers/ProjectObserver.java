61688c9024ed0bc8eb807c1ba53b4435f43f6214
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2024-12-03 14:31:12 +0100

    feat(): ProjectObserver update made

--- src/main/java/ecostruxure/rate/calculator/bll/team/observers/ProjectObserver.java
+++ src/main/java/ecostruxure/rate/calculator/bll/team/observers/ProjectObserver.java
@@ -1,4 +1,26 @@
 package ecostruxure.rate.calculator.bll.team.observers;
 
-public class ProjectObserver {
+import ecostruxure.rate.calculator.be.Project;
+import ecostruxure.rate.calculator.be.Team;
+import ecostruxure.rate.calculator.bll.team.ITeamObserver;
+import ecostruxure.rate.calculator.bll.utils.RateUtils;
+import ecostruxure.rate.calculator.dal.IProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectObserver implements ITeamObserver {
+    @Autowired
+    private IProjectRepository projectRepository;
+
+    @Override
+    public void update(Team team) {
+        List<Project> projects = projectRepository.findProjectsByTeamId(team.getTeamId());
+        for (Project project : projects){
+            var updatedProject = RateUtils.updateProjectRates(project);
+            projectRepository.save(updatedProject);
+        }
+    }
 }
--- src/main/java/ecostruxure/rate/calculator/bll/utils/RateUtils.java
+++ src/main/java/ecostruxure/rate/calculator/bll/utils/RateUtils.java
@@ -1,9 +1,14 @@
 package ecostruxure.rate.calculator.bll.utils;
 
 import ecostruxure.rate.calculator.be.Profile;
+import ecostruxure.rate.calculator.be.Project;
+import ecostruxure.rate.calculator.be.ProjectTeam;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Objects;
 
@@ -13,6 +18,96 @@ public class RateUtils {
     private static final int GENERAL_SCALE = 2;
     private static final RoundingMode ROUNDING_MODE = RoundingMode.HALF_UP;
     private static final BigDecimal HUNDRED = new BigDecimal("100.00");
+
+    // Method to calculate the effective work hours based on effectiveness percentage
+    public static BigDecimal effectiveWorkHours(Profile profile) {
+        // If annual hours is zero, return zero to avoid division by zero
+        if (profile.getAnnualHours().compareTo(BigDecimal.ZERO) == 0) {
+            return BigDecimal.ZERO.setScale(GENERAL_SCALE, ROUNDING_MODE);
+        }
+        BigDecimal effectiveness = profile.getEffectivenessPercentage().divide(HUNDRED, GENERAL_SCALE, ROUNDING_MODE);
+        // Calculate the effective work hours based on annual hours and effectiveness percentage
+        BigDecimal effectiveWorkHours = profile.getAnnualHours().multiply(effectiveness).setScale(GENERAL_SCALE, ROUNDING_MODE);
+
+        return effectiveWorkHours;
+    }
+
+    public static Project updateProjectRates(Project project) {
+        var projectContainsMembers = project.getProjectTeams() != null;
+        var projectContainsDayRate = project.getProjectDayRate() != null;
+        var projectIsStarted = LocalDate.now().isAfter(project.getProjectStartDate());
+
+        // Calculate, if rest cost have been calc. before
+        if (projectContainsMembers && projectContainsDayRate && project.getProjectRestCostDate() == null) {
+            project.setProjectTotalCostAtChange(calculateTotalCostAtChangeFirstTime(project));
+            project.setProjectRestCostDate(LocalDate.now());
+            project.setProjectDayRate(calculateDayRate(project.getProjectTeams()));
+            project.setProjectGrossMargin(calculateGrossMargin(project));
+        }
+        // Calculate, if project members are present and project is started
+        else if (projectContainsMembers && projectContainsDayRate && projectIsStarted) {
+            project.setProjectTotalCostAtChange(calculateTotalCostAtChange(project));
+            project.setProjectRestCostDate(LocalDate.now());
+            project.setProjectDayRate(calculateDayRate(project.getProjectTeams()));
+            project.setProjectGrossMargin(calculateGrossMargin(project));
+        }
+        // Calculate, if project members are present and project is not started
+        else if (projectContainsMembers && projectContainsDayRate) {
+            project.setProjectDayRate(calculateDayRate(project.getProjectTeams()));
+            project.setProjectGrossMargin(calculateGrossMargin(project));
+        }
+
+        project.setProjectTotalDays(calculateWorkingDays(project.getProjectStartDate(), project.getProjectEndDate()));
+        return project;
+    }
+
+    private static BigDecimal calculateTotalCostAtChangeFirstTime(Project project) {
+        BigDecimal totalCostAtChange;
+        var daysPassed = LocalDate.now().toEpochDay() - project.getProjectStartDate().toEpochDay();
+        totalCostAtChange = project.getProjectDayRate().multiply(BigDecimal.valueOf(daysPassed));
+        return totalCostAtChange;
+    }
+
+    private static BigDecimal calculateTotalCostAtChange(Project project) {
+        BigDecimal totalCostAtChange = project.getProjectTotalCostAtChange();
+        var daysPassed = LocalDate.now().toEpochDay() - project.getProjectRestCostDate().toEpochDay();
+        totalCostAtChange = totalCostAtChange.add(project.getProjectDayRate().multiply(BigDecimal.valueOf(daysPassed)));
+        return totalCostAtChange;
+    }
+
+    private static BigDecimal calculateDayRate(List<ProjectTeam> projectTeams) {
+        BigDecimal totalDayRate = BigDecimal.ZERO;
+        for (ProjectTeam projectTeam : projectTeams) {
+            BigDecimal markup = projectTeam.getTeam().getMarkupPercentage();
+            var dayRateWithMarkup = projectTeam.getTeam().getDayRate()
+                    .multiply(markup.divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP).add(BigDecimal.ONE));
+            var allocatedDayRate = dayRateWithMarkup.multiply(projectTeam.getAllocationPercentage()
+                    .divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP));
+            projectTeam.getTeam().setDayRate(allocatedDayRate);
+            totalDayRate = totalDayRate.add(allocatedDayRate);
+        }
+        return totalDayRate;
+    }
+
+    private static int calculateWorkingDays(LocalDate startDate, LocalDate endDate) {
+        int count = 0;
+        LocalDate currentDate = startDate;
+        while (!currentDate.isAfter(endDate)) {
+            DayOfWeek dayOfWeek = currentDate.getDayOfWeek();
+            if (dayOfWeek != DayOfWeek.SATURDAY && dayOfWeek != DayOfWeek.SUNDAY) {
+                count++;
+            }
+            currentDate = currentDate.plusDays(1);
+        }
+        return count;
+    }
+
+    private static BigDecimal calculateGrossMargin(Project project) {
+        BigDecimal grossMarginNumber = project.getProjectPrice().subtract(project.getProjectDayRate().multiply(BigDecimal.valueOf(project.getProjectTotalDays())));
+        BigDecimal grossMarginPercentage = grossMarginNumber.divide(project.getProjectPrice(), 2, RoundingMode.HALF_UP).multiply(BigDecimal.valueOf(100));
+        return grossMarginPercentage;
+    }
+}/*
 
     // Method to calculate the hourly rate for a given profile
     public static BigDecimal hourlyRate(Profile profile) {
@@ -82,18 +177,7 @@ public class RateUtils {
         return profile.getAnnualHours().multiply(percentageAsDecimal);
     }
 
-    // Method to calculate the effective work hours based on effectiveness percentage
-    public static BigDecimal effectiveWorkHours(Profile profile) {
-        // If annual hours is zero, return zero to avoid division by zero
-        if (profile.getAnnualHours().compareTo(BigDecimal.ZERO) == 0) {
-            return BigDecimal.ZERO.setScale(GENERAL_SCALE, ROUNDING_MODE);
-        }
-        BigDecimal effectiveness = profile.getEffectivenessPercentage().divide(HUNDRED, GENERAL_SCALE, ROUNDING_MODE);
-        // Calculate the effective work hours based on annual hours and effectiveness percentage
-        BigDecimal effectiveWorkHours = profile.getAnnualHours().multiply(effectiveness).setScale(GENERAL_SCALE, ROUNDING_MODE);
 
-        return effectiveWorkHours;
-    }
 
     // Method to calculate the effectiveness percentage for the profile
     public static BigDecimal effectiveness(Profile profile) {
@@ -184,3 +268,5 @@ public class RateUtils {
         return totalHours.multiply(percentageAsDecimal);
     }
 }
+*/
+
