dae0b80eaf7fe0d0d010acb5a72041cef0d9be23
Lasse Høeg <93433669+hoeeg79@users.noreply.github.com>
2024-12-04 09:25:02 +0100

    refactor(sql): removed some unused functions

--- db-init/3_functions/project_team_update_trigger.sql
+++
@@ -1,7 +0,0 @@
--- Using function from update_project_on_team_change,
--- since its same stuff needed to be done.
-
-CREATE TRIGGER project_team_update_trigger
-    AFTER INSERT OR UPDATE OR DELETE ON project_team
-    FOR EACH ROW
-EXECUTE FUNCTION update_project_on_team_change();
