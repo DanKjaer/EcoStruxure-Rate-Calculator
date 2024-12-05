bf3256ed55de06dbf6b219c3226d0b91201f6215
Peder Antonisen <113884778+ShadySloth@users.noreply.github.com>
2024-12-05 13:53:05 +0100

    refactor: removed commented code

--- src/main/java/ecostruxure/rate/calculator/controllers/ExcelController.java
+++ src/main/java/ecostruxure/rate/calculator/controllers/ExcelController.java
@@ -1,18 +1,10 @@
 package ecostruxure.rate.calculator.controllers;
 
-import ecostruxure.rate.calculator.be.Team;
 import ecostruxure.rate.calculator.bll.service.ExcelService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
-import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/excel")
@@ -28,26 +20,4 @@ public class ExcelController {
     public ResponseEntity<byte[]> getExcel() throws Exception {
         return excelService.exportToExcel();
     }
-
-//    @GetMapping("/team")
-//    public ResponseEntity<byte[]> getTeamExcel(@RequestParam UUID teamId) throws Exception {
-//        ByteArrayOutputStream out = excelService.exportTeam(teamId);
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
-//        headers.setContentDispositionFormData("attachment", "data.xlsx");
-//
-//        return new ResponseEntity<>(out.toByteArray(), headers, HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/teams")
-//    public ResponseEntity<byte[]> getTeamsExcel(@RequestBody List<Team> teamList) throws Exception {
-//        ByteArrayOutputStream out = excelService.exportTeams(teamList);
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
-//        headers.setContentDispositionFormData("attachment", "data.xlsx");
-//
-//        return new ResponseEntity<>(out.toByteArray(), headers, HttpStatus.OK);
-//    }
 }
