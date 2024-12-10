b315ff0b74305444d41f2333ebf6ba86fb968f01
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2024-12-10 13:43:47 +0100

    feat(): UI for login updated, so it matches our current ui

--- src/FE/src/app/pages/login-page/login-page.component.ts
+++ src/FE/src/app/pages/login-page/login-page.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {MatDialogActions, MatDialogContent, MatDialogTitle} from '@angular/material/dialog';
+import {MatButton} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {NgForOf} from '@angular/common';
+import {TranslateModule} from '@ngx-translate/core';
+import {MatInput} from '@angular/material/input';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    MatDialogActions,
+    MatButton,
+    MatFormFieldModule,
+    NgForOf,
+    TranslateModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatInput
   ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
@@ -15,6 +29,6 @@ export class LoginPageComponent {
   password: any;
 
   onSubmit() {
-    
+
   }
 }
